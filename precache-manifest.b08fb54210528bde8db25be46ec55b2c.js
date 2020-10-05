86e94075b7ca91c97989e76eb9a8aa75e6cb0ddc
Rohini210 <prohini210@gmail.com>
2020-10-05 11:28:09 +0530

    Updates

---
+++ precache-manifest.b08fb54210528bde8db25be46ec55b2c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2ba6890f821c4ed05c18dc25a7d3c7",
+    "url": "/Web-Page/index.html"
+  },
+  {
+    "revision": "6990976084f6892f1829",
+    "url": "/Web-Page/static/css/main.229054a2.chunk.css"
+  },
+  {
+    "revision": "8c5b5bcdcf20707ff05e",
+    "url": "/Web-Page/static/js/2.7cc6d499.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Web-Page/static/js/2.7cc6d499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6990976084f6892f1829",
+    "url": "/Web-Page/static/js/main.03769bbf.chunk.js"
+  },
+  {
+    "revision": "d88714d7746bc2f22777",
+    "url": "/Web-Page/static/js/runtime-main.16ca8135.js"
+  },
+  {
+    "revision": "f583096db7f75c4b5dbf6c2dff63291e",
+    "url": "/Web-Page/static/media/Bell.f583096d.jpg"
+  },
+  {
+    "revision": "e92878ecf50ac09810bff0ce4089aa9d",
+    "url": "/Web-Page/static/media/Boy.e92878ec.jpg"
+  },
+  {
+    "revision": "4f4fc4b1f53bab2922cc1a86b684dd6b",
+    "url": "/Web-Page/static/media/DP.4f4fc4b1.jpg"
+  },
+  {
+    "revision": "e4ac6760c1781fea5d2d61dc18a60009",
+    "url": "/Web-Page/static/media/pencil.e4ac6760.jpg"
+  },
+  {
+    "revision": "5af5db9bcfb235d8b8d45a425d6d5d06",
+    "url": "/Web-Page/static/media/photograph.5af5db9b.jpg"
+  }
+]);
